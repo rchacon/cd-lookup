9de4f7d72d8c58ee79f740f48b34c18bf82425d0
Raul Chacon <chaconrj@gmail.com>
2026-08-06 20:03:50 -0700

    Bump version to 0.4.0

--- cd-lookup/cd-lookup.php
+++ cd-lookup/cd-lookup.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: CD Lookup
  * Description: Look up congressional representatives for a given street address.
- * Version:     0.3.1
+ * Version:     0.4.0
  * Requires at least: 6.0
  * Requires PHP: 8.0
  * Author:      Raul Chacon
